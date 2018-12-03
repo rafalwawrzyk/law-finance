59ea694f9b8d128c0446c0e10ab997f7c7586ddb
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2018-12-03 21:37:45 +0100

    Fix drawer bug
    Add additional style to body cause its collapse with
    drawer styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,22 @@ import Header from '../styles/blocks/Banner'
 import HeaderButton from '../styles/elements/Button'
 import Layout from '../components/layout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { createGlobalStyle } from 'styled-components'
 
+
+
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding-right:0 !important;
+  }
+`
 
 
 const IndexPage = props => {
   return (
     <Layout>
+      <GlobalStyle/>
       <Header>
         <Header.Title>Kancelaria Prawno-Finansowa</Header.Title>
         <Header.SubTitle>Beata Murszewska</Header.SubTitle>
